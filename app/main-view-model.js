fd20994b226697a3f86ccf5050d6e27759473a07
Deepak Arora <deepak.arora76@gmail.com>
2021-03-13 15:43:56 +0100

    Update the demo code to use nativescript-dna-deviceinfo 3.6.0

--- app/main-view-model.js
+++ app/main-view-model.js
@@ -30,12 +30,17 @@ async function showDeviceInfo() {
   console.log("Storage volume info: ", DeviceInfo.storageVolumes());
   console.log("WiFi SSID: ", DeviceInfo.wifiSSID());
   console.log("Display metrics: ", DeviceInfo.displayMetrics());
+  console.log("Mic available: ", DeviceInfo.isMicAvailable());
+  console.log("Connected to Bluetooth Headset:", DeviceInfo.isBluetoothHeadsetConnected());
   console.log("Is portrait orientation: ", DeviceInfo.isPortrait());
   console.log("Is tablet: ", DeviceInfo.isTablet());
   console.log("Is 24 hour: ", DeviceInfo.is24Hour());
   console.log("Is emulator: ", DeviceInfo.isEmulator());
   console.log("Is battery charing: ", DeviceInfo.isBatteryCharging());
   console.log("Is Location service enabled: ", await DeviceInfo.isLocationEnabled().catch(error => console.log(error)));
+  console.log("Setting audio volume level to 50");
+  DeviceInfo.setAudioVolumeLevel(50);
+  console.log("Audio output volume level: ", DeviceInfo.audioVolumeLevel());
 
   if (DeviceInfo.systemManufacturer() !== "Apple") {
     requestPermissions([
