d98d5ed1b1e7e8fbef0739e0dc1d06f9caca396c
Deepak Arora <deepak.arora76@gmail.com>
2021-10-23 11:21:58 +0200

    Updated the demo app to use latest Nativescript Device Info package

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,11 +1,11 @@
-import { NativeScriptConfig } from '@nativescript/core';
+import { NativeScriptConfig } from '@nativescript/core'
 
 export default {
-  id: 'org.nativescript.HelloWorld',
+  id: 'org.nativescript.TestAppDeviceInfo',
   appPath: 'app',
   appResourcesPath: 'App_Resources',
   android: {
     v8Flags: '--expose_gc',
-    markingMode: 'none'
-  }
-} as NativeScriptConfig;
+    markingMode: 'none',
+  },
+} as NativeScriptConfig
